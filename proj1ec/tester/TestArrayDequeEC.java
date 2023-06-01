95610b1532f8980f07c1023f0083a63145d23147
akfc58 <akfc58@gmail.com>
2023-06-01 16:58:03 +0800

    try proj1 ec ver1

--- proj1ec/tester/TestArrayDequeEC.java
+++ proj1ec/tester/TestArrayDequeEC.java
@@ -1,6 +1,7 @@
 package tester;
 
 import static org.junit.Assert.*;
+
 import org.junit.Test;
 import student.StudentArrayDeque;
 import edu.princeton.cs.algs4.StdRandom;
@@ -11,8 +12,11 @@ public class TestArrayDequeEC {
     @Test
     public void randomizedArrayDequeTest() {
         StudentArrayDeque<Integer> LLD1 = new StudentArrayDeque<>();
-        ArrayDequeSolution<Integer> LLD2 =new ArrayDequeSolution<>();
-        int N = 5000;
+        ArrayDequeSolution<Integer> LLD2 = new ArrayDequeSolution<>();
+        int N = 500000;
+        ArrayDequeSolution<String> stack = new ArrayDequeSolution<>();
+        stack.addLast("");
+        stack.addLast("");
         for (int i = 0; i < N; i += 1) {
             int operationNumber = StdRandom.uniform(0, 6);
             if (operationNumber == 0) {
@@ -20,33 +24,46 @@ public class TestArrayDequeEC {
                 int randVal = StdRandom.uniform(0, 10000);
                 LLD1.addLast(randVal);
                 LLD2.addLast(randVal);
+                stack.addLast("addLast(" + randVal + ")");
             } else if (operationNumber == 1) {
                 // addFirst
                 int randVal = StdRandom.uniform(0, 10000);
                 LLD1.addFirst(randVal);
                 LLD2.addFirst(randVal);
+                stack.addLast("addFirst(" + randVal + ")");
             } else if (operationNumber == 2) {
                 // size
                 int size1 = LLD1.size();
                 int size2 = LLD2.size();
-                assertEquals(size1, size2);
+                stack.addLast("size()");
+                assertEquals(stack.get(stack.size() - 1) + "\n" + stack.get(stack.size() - 2) + "\n"
+                        + stack.get(stack.size() - 3) + "\n", size1, size2);
             } else if (operationNumber == 3) {
                 // removeLast
                 if (LLD1.size() > 0) {
-                    assertEquals(LLD1.removeLast(), LLD2.removeLast());
+                    assertEquals(stack.get(stack.size() - 1) + "\n" + stack.get(stack.size() - 2) + "\n"
+                            + stack.get(stack.size() - 3) + "\n", LLD1.removeLast(), LLD2.removeLast());
+                    stack.addLast("removeLast()");
                 }
             } else if (operationNumber == 4) {
                 // removeFirst
                 if (LLD1.size() > 0) {
-                    assertEquals(LLD1.removeFirst(), LLD2.removeFirst());
+                    assertEquals(stack.get(stack.size() - 1) + "\n" + stack.get(stack.size() - 2) + "\n"
+                            + stack.get(stack.size() - 3) + "\n", LLD1.removeFirst(), LLD2.removeFirst());
+                    stack.addLast("removeFirst()");
                 }
             } else if (operationNumber == 5) {
-                if ( LLD1.size() > 0) {
+                if (LLD1.size() > 0) {
                     int index = StdRandom.uniform(0, LLD1.size());
-                    assertEquals(LLD1.get(index), LLD2.get(index));
+                    stack.addLast("get(" + index + ")");
+                    assertEquals(stack.get(stack.size() - 1) + "\n" + stack.get(stack.size() - 2) + "\n"
+                            + stack.get(stack.size() - 3) + "\n", LLD1.get(index), LLD2.get(index));
                 }
             }
         }
+    }
+    public static void main(String[] args) {
+        jh61b.junit.TestRunner.runTests(TestArrayDequeEC.class);
     }
 
 }
