667549354e27f7cb36421cc6e1f1e062ec3f6a96
akfc58 <akfc58@gmail.com>
2023-06-01 17:56:59 +0800

    try proj1 ec ver5

--- proj1ec/tester/TestArrayDequeEC.java
+++ proj1ec/tester/TestArrayDequeEC.java
@@ -16,9 +16,7 @@ public class TestArrayDequeEC {
         StudentArrayDeque<Integer> LLD1 = new StudentArrayDeque<>();
         ArrayDequeSolution<Integer> LLD2 = new ArrayDequeSolution<>();
         int N = 500000;
-        ArrayList<String> stack = new ArrayList<>();
-        stack.add("");
-        stack.add("");
+        String stack = "";
         for (int i = 0; i < N; i += 1) {
             int operationNumber = StdRandom.uniform(0, 6);
             if (operationNumber == 0) {
@@ -26,40 +24,37 @@ public class TestArrayDequeEC {
                 int randVal = 666;
                 LLD1.addLast(randVal);
                 LLD2.addLast(randVal);
-                stack.add("addLast(" + randVal + ")");
+                stack += "addLast(" + randVal + ")\n";
             } else if (operationNumber == 1) {
                 // addFirst
                 int randVal = 666;
                 LLD1.addFirst(randVal);
                 LLD2.addFirst(randVal);
-                stack.add("addFirst(" + randVal + ")");
+                stack += "addFirst(" + randVal + ")\n";
             } else if (operationNumber == 2) {
                 // size
                 int size1 = LLD1.size();
                 int size2 = LLD2.size();
-                stack.add("size()");
-                assertEquals(stack.get(stack.size() - 3) + "\n" + stack.get(stack.size() - 2) + "\n"
-                        + stack.get(stack.size() - 1), size1, size2);
+                stack += "size()\n";
+                assertEquals(stack, size1, size2);
             } else if (operationNumber == 3) {
                 // removeLast
                 if (LLD1.size() > 0) {
-                    stack.add("removeLast()");
-                    assertEquals(stack.get(stack.size() - 3) + "\n" + stack.get(stack.size() - 2) + "\n"
-                            + stack.get(stack.size() - 1), LLD1.removeLast(), LLD2.removeLast());
+                    stack += "removeLast()\n";
+                    assertEquals(stack, LLD1.removeLast(), LLD2.removeLast());
                 }
             } else if (operationNumber == 4) {
                 // removeFirst
                 if (LLD1.size() > 0) {
-                    stack.add("removeFirst()");
-                    assertEquals(stack.get(stack.size() - 3) + "\n" + stack.get(stack.size() - 2) + "\n"
-                            + stack.get(stack.size() - 1), LLD1.removeFirst(), LLD2.removeFirst());
+                    stack += "removeFirst()\n";
+                    assertEquals(stack, LLD1.removeFirst(), LLD2.removeFirst());
                 }
             } else if (operationNumber == 5) {
+                // get
                 if (LLD1.size() > 0) {
                     int index = StdRandom.uniform(0, LLD1.size());
-                    stack.add("get(" + index + ")");
-                    assertEquals(stack.get(stack.size() - 3) + "\n" + stack.get(stack.size() - 2) + "\n"
-                            + stack.get(stack.size() - 1), LLD1.get(index), LLD2.get(index));
+                    stack += "get(" + index + ")\n";
+                    assertEquals(stack, LLD1.get(index), LLD2.get(index));
                 }
             }
         }
