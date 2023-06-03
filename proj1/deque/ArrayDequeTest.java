29f4ca9cf436ed8c252ea85c5832006b4424d82c
akfc58 <akfc58@gmail.com>
2023-06-03 09:59:41 +0800

    try proj1 resize again. inital.

--- proj1/deque/ArrayDequeTest.java
+++ proj1/deque/ArrayDequeTest.java
@@ -1,5 +1,6 @@
 package deque;
 
+import edu.princeton.cs.algs4.StdRandom;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
@@ -17,5 +18,48 @@ public class ArrayDequeTest {
         t2.addFirst("middle");
         t2.addFirst("first");
         assertEquals(t1, t2);
+    }
+
+    /** A big size random test for almost all operations in AD. */
+    @Test
+    public void randomizedArrayDequeTest() {
+        ArrayDeque<Integer> LLD1 = new ArrayDeque<>();
+        ArrayDeque<Integer> LLD2 = new ArrayDeque<>();
+        int N = 500000;
+        for (int i = 0; i < N; i += 1) {
+            int operationNumber = StdRandom.uniform(0, 6);
+            if (operationNumber == 0) {
+                // addLast
+                int randVal = 666;
+                LLD1.addLast(randVal);
+                LLD2.addLast(randVal);
+            } else if (operationNumber == 1) {
+                // addFirst
+                int randVal = 666;
+                LLD1.addFirst(randVal);
+                LLD2.addFirst(randVal);
+            } else if (operationNumber == 2) {
+                // size
+                int size1 = LLD1.size();
+                int size2 = LLD2.size();
+                assertEquals(size1, size2);
+            } else if (operationNumber == 3) {
+                // removeLast
+                if (LLD1.size() > 0) {
+                    assertEquals(LLD1.removeLast(), LLD2.removeLast());
+                }
+            } else if (operationNumber == 4) {
+                // removeFirst
+                if (LLD1.size() > 0) {
+                    assertEquals(LLD1.removeFirst(), LLD2.removeFirst());
+                }
+            } else if (operationNumber == 5) {
+                // get
+                if (LLD1.size() > 0) {
+                    int index = StdRandom.uniform(0, LLD1.size());
+                    assertEquals(LLD1.get(index), LLD2.get(index));
+                }
+            }
+        }
     }
 }
